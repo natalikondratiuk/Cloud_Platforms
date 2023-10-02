90cf9943b37bd34e966bda5d204f1842aa342a35
natalikondratiuk <71512255+natalikondratiuk@users.noreply.github.com>
2023-10-02 18:45:26 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -3,7 +3,5 @@ import random
 
 def index_day() :
     i = random.randint(0, 9)
-    try :
-        get_next_day(i)
-    except IndexError:
-        print('Index is out of range: expected indexes in range 0...6')
+    if i >= 0 and i <= 6: get_next_day(i)
+    else: print('Index is out of range: expected indexes in range 0...6')
