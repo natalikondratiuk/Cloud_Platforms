fff1afd577fc4781854161a099eb502b5d3274fc
natalikondratiuk <71512255+natalikondratiuk@users.noreply.github.com>
2023-10-03 12:12:28 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,3 +1,5 @@
+from app import get_next_day
+
 dict_test = {
     0: "Понеділок",
     1: "Вівторок",
@@ -7,13 +9,12 @@ dict_test = {
     5: "Субота",
     6: "Неділя"
 }
+get_next_day(0)
 
-'''
 def monday_test() : assert dict_test.get(0) == "Понеділок"
 def tuesday_test() : assert dict_test.get(1) == "Вівторок"
 def wednesday_test() : assert dict_test.get(2) == "Середа"
 def thursday_test() : assert dict_test.get(3) == "Четвер"
 def friday_test() : assert dict_test.get(4) == "П'ятниця"
 def saturday_test() : assert dict_test.get(5) == "Субота"
-'''
 def sunday_test() : assert dict_test.get(6) == "Неділя"
