853f406cab99841a8b4244391dd5b8159e7f2a7a
natalikondratiuk <71512255+natalikondratiuk@users.noreply.github.com>
2023-10-03 10:57:14 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,7 +1,17 @@
-from Cloud_Platforms.app import get_next_day
-import random
+dict_test = {
+    0: "Понеділок",
+    1: "Вівторок",
+    2: "Середа",
+    3: "Четвер",
+    4: "П'ятниця",
+    5: "Субота",
+    6: "Неділя"
+}
 
-def index_day() :
-    i = random.randint(0, 9)
-    if i >= 0 and i <= 6: get_next_day(i)
-    else: print('Index is out of range: expected indexes in range 0...6')
+def monday_test() : assert dict_test.get(0) == "Понеділок"
+def tuesday_test() : assert dict_test.get(1) == "Вівторок"
+def wednesday_test() : assert dict_test.get(2) == "Середа"
+def thursday_test() : assert dict_test.get(3) == "Четвер"
+def friday_test() : assert dict_test.get(4) == "П'ятниця"
+def saturday_test() : assert dict_test.get(5) == "Субота"
+def sunday_test() : assert dict_test.get(6) == "Неділя"
